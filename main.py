763f8c51894a34c4878ace13127f6cdfba79ea86
brogment <140029632+brogment@users.noreply.github.com>
2025-02-25 15:02:58 -0600

    Get response and convert to JSON

--- main.py
+++ main.py
@@ -12,9 +12,24 @@
 
 import os
 from dotenv import load_dotenv
+import requests
+import pandas as pd
+import json
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+start_date = "2025-01-01"
+end_date = "2025-01-07"
 
-print(f"API_KEY: {API_KEY}")
+url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={API_KEY}"
+response = requests.get(url)
+data = response.json()
+
+# print(json.dumps(data, indent=2))
+
+# with open("neo_data.json","w") as f:
+#     json.dump(data, f, indent=2)
+
+asteroids = data["near_earth_objects"][start_date]
+print(f"Number of asteroids on {start_date}: {len(asteroids)}")
